339ef7132e8df79c09e65927655836e740c7700a
Qubasa <consulting@qube.email>
2025-06-12 17:29:27 +0200

    Set disk schema of machine: demo to single-disk

--- machines/demo/disko.nix
+++ machines/demo/disko.nix
@@ -1,29 +1,21 @@
+# ---
+# schema = "single-disk"
+# [placeholders]
+# mainDisk = "/dev/disk/by-path/pci-0000:04:00.0" 
+# ---
+# This file was automatically generated!
+# CHANGING this configuration requires wiping and reinstalling the machine
 {
-  lib,
-  clan-core,
-  config,
-  ...
-}:
 
-let
-  suffix = config.clan.core.vars.generators.disk-id.files.diskId.value;
-in
-{
-  imports = [
-    clan-core.clanModules.disk-id
-  ];
-
-  # TO NOT EDIT THIS FILE AFTER INSTALLATION of a machine
-  # Otherwise your system might not boot because of missing partitions / filesystems
-  boot.loader.grub.efiSupport = lib.mkDefault true;
-  boot.loader.grub.efiInstallAsRemovable = lib.mkDefault true;
+  boot.loader.grub.efiSupport = true;
+  boot.loader.grub.efiInstallAsRemovable = true;
+  boot.loader.grub.enable = true;
   disko.devices = {
     disk = {
-      "main" = {
-        name = "main-" + suffix;
+      main = {
+        name = "main-07746e3cb5d449ee886c89cc00308470";
+        device = "/dev/disk/by-path/pci-0000:04:00.0";
         type = "disk";
-        # Set the following in flake.nix for each maschine:
-        # device = <uuid>;
         content = {
           type = "gpt";
           partitions = {
@@ -32,23 +24,21 @@ in
               type = "EF02"; # for grub MBR
               priority = 1;
             };
-            "ESP" = {
-              size = "512M";
+            ESP = {
               type = "EF00";
+              size = "500M";
               content = {
                 type = "filesystem";
                 format = "vfat";
                 mountpoint = "/boot";
-                mountOptions = [ "nofail" ];
+                mountOptions = [ "umask=0077" ];
               };
             };
-            "root" = {
+            root = {
               size = "100%";
               content = {
                 type = "filesystem";
                 format = "ext4";
-                # format = "btrfs";
-                # format = "bcachefs";
                 mountpoint = "/";
               };
             };
