e3b1386b0cfca5a4fc957107ed1d91f005e90105
Qubasa <consulting@qube.email>
2026-09-22 10:51:21 +0200

    Provide code as VSCodium and use it as git difftool

--- machines/wintux/vscode.nix
+++ machines/wintux/vscode.nix
@@ -1,27 +1,33 @@
 { unstablePkgs, pkgs, ... }:
 
 let
-  vscode = unstablePkgs.vscode-with-extensions;
+  codium = unstablePkgs.vscode-with-extensions.override {
+    vscode = pkgs.vscodium;
+    vscodeExtensions =
+      with pkgs.open-vsx;
+      [
+        ms-python.python
+        llvm-vs-code-extensions.vscode-clangd
+        yzhang.markdown-all-in-one
+        jnoortheen.nix-ide
+        alefragnani.bookmarks
+        tamasfe.even-better-toml
+        james-yu.latex-workshop
+        hashicorp.terraform
+        matangover.mypy
+        rust-lang.rust-analyzer
+        jjk.jjk
+      ];
+  };
 in
 {
   environment.systemPackages = [
-    (vscode.override {
-      vscode = pkgs.vscodium;
-      vscodeExtensions =
-        with pkgs.open-vsx;
-        [
-          ms-python.python
-          llvm-vs-code-extensions.vscode-clangd
-          yzhang.markdown-all-in-one
-          jnoortheen.nix-ide
-          alefragnani.bookmarks
-          tamasfe.even-better-toml
-          james-yu.latex-workshop
-          hashicorp.terraform
-          matangover.mypy
-          rust-lang.rust-analyzer
-          jjk.jjk
-        ];
-    })
+    codium
+    # `code` is what git, jj and every tutorial call; keep it pointing at
+    # the same VSCodium build that carries the extensions.
+    (pkgs.runCommand "code-codium-alias" { } ''
+      mkdir -p $out/bin
+      ln -s ${codium}/bin/codium $out/bin/code
+    '')
   ];
 }
